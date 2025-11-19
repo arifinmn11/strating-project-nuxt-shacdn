4e6cffb3274778aa050fb76e4973936d39ce72f3
arifin <arifin@tbk.co.id>
2025-11-19 07:53:13 +0700

    init project

---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+import tailwindcss from '@tailwindcss/vite'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    'shadcn-nuxt',
+    '@pinia/nuxt',
+    'nuxt-lucide-icons',
+    '@nuxtjs/color-mode',
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "~/components/ui"
+     */
+    componentDir: '~/components/ui',
+  },
+})
