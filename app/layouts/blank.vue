b00ce28c0e7dbb1bd9cc8c2fb6467003becaa7b7
arifin <arifin@tbk.co.id>
2025-12-12 09:23:58 +0700

    init

--- app/layouts/blank.vue
+++ app/layouts/blank.vue
@@ -1,12 +1,10 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
+    Blank Layout
     <slot />
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,17 +2,10 @@
 </script>
 
 <template>
-  <SidebarProvider>
-    <LayoutAppSidebar />
-    <SidebarInset>
-      <LayoutHeader />
-      <div class="flex flex-col flex-1">
-        <div class="@container/main p-4 lg:p-6 grow">
-          <slot />
-        </div>
-      </div>
-    </SidebarInset>
-  </SidebarProvider>
+   <div>
+    Defalt Layout
+      <slot />
+    </div>
 </template>
 
 <style scoped>
